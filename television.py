35447afe26c54b7e8a60db429458ed88af5b69dc
LaurelleS <laurelle887@gmail.com>
2023-11-20 14:51:27 -0600

    Television class

---
+++ television.py
@@ -0,0 +1,60 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+
+    def power(self):
+        if self.__status:
+            self.__status = False
+        else:
+            self.__status = True
+
+    def mute(self):
+        if self.__status:
+            if self.__muted:
+                self.__status = False
+            else:
+                self.__muted = True
+
+    def channel_up(self):
+        if self.__status:
+            if self.__channel == self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
+            else:
+                self.__channel += 1
+
+    def channel_down(self):
+        if self.__status:
+            if self.__channel == self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+            else:
+                self.__channel -= 1
+
+    def volume_up(self):
+        self.__muted = False
+        if self.__status:
+            if self.__volume == self.MAX_VOLUME:
+                return
+            else:
+                self.__volume += 1
+
+    def volume_down(self):
+        self.__muted = False
+        if self.__status:
+            if self.__volume == self.MIN_VOLUME:
+                return
+            else:
+                self.__volume -= 1
+
+    def __str__(self):
+        if not self.__muted:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
+        else:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = 0'
